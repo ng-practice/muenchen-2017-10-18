be2a88013745a3ae60c8f2c224c7c5e3c10988a4
Gregor Woiwode <gregor.woiwode@gmail.com>
2017-10-18 14:10:23 +0200

    (playground) add component info-box

--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InfoBoxComponent } from './info-box/info-box.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [InfoBoxComponent],
+  exports: [InfoBoxComponent]
 })
 export class PlaygroundModule { }
