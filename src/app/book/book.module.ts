ae32e0067ce865d2f3cd96babe480ed480afc1ff
Gregor Woiwode <gregor.woiwode@gmail.com>
2017-10-19 11:51:02 +0200

    use HttpClient over static data

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -1,11 +1,14 @@
-import { BookDataService } from './../shared/book-data.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { BookDataService } from './../shared/book-data.service';
 import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   declarations: [BookListComponent],
   exports: [BookListComponent],
